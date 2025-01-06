6b2b275623745e3855b50cf3ca85a23763c2ca11
Himanshu <sagarhimanshu472@gmail.com>
2025-01-06 23:41:21 +0530

    /albums POST request endpoint complete!

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -14,19 +14,32 @@ type album struct {
 }
 
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Garry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: "56.99"},
+    {ID: "2", Title: "Jeru", Artist: "Garry Mulligan", Price: "17.99"},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: "39.99"},
 }
 
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
 }
 
 // getAlbums response with the list of all the albums as JSON
 func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOk, albums)
+    c.JSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON recieved in the request body
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+    
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
 }
