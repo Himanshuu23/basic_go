448641805f180bedad005c3ba352043a6c9aa0a9
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-07 17:31:56 +0530

    feat: completed the web service project

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -22,6 +22,7 @@ var albums = []album{
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
 
     router.Run("localhost:8080")
