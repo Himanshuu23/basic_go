6fd7b296d462c2f688059289982bc1f42d202153
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-07 20:01:58 +0530

    generics stuff

---
+++ generics/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	int64 | float64
+}
+
+func main() {
+	ints := map[string]int64{
+		"first": 64,
+		"second": 36,
+	}
+
+	floats := map[string]float64{
+		"first": 63.82,
+		"second": 36.18,
+	}
+
+	fmt.Printf("Generics Sums: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+}
+
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
+
+func SumIntsOrFloats[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+
+	return s
+}
