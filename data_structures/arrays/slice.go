d48943f16733c82675d0d76ee9a557f1c71cf743
Himanshu <sagarhimanshu472@gmail.com>
2025-01-12 00:13:43 +0530

    array: deleting at a particular index

--- data_structures/arrays/slice.go
+++ data_structures/arrays/slice.go
@@ -4,11 +4,17 @@ import "fmt"
 
 func main() {
 	Array := []int{1, 2, 3, 4, 5}
-	array := add(3, 2, Array)
-	fmt.Println(array)
+	array1 := add(3, 2, Array)
+	array2 := remove(2, Array)
+	fmt.Println(array1, array2)
 }
 
 func add(element int, index int, array []int) []int {
 	array = append(array[:index], append([]int{element}, array[index:]...)...)
+	return array
+}
+
+func remove(index int, array []int) []int {
+	array = append(array[:index], array[index+1:]...)
 	return array
 }
