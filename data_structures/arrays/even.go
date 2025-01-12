c1c7573985615bf6fa7c3564c231bb46d84ea4a0
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-12 10:56:04 +0530

    array: even done

--- data_structures/arrays/even.go
+++ data_structures/arrays/even.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main() {
 	array := []int{1, 2, 3, 4, 5, 6}
-	New := remove(array, 1)
+	New := even(array)
 	fmt.Println(New)
 }
 
-func remove(array int[], index int) []int {
+func remove(array []int, index int) []int {
 	Array := append(array[:index], array[index+1:]...)
 	return Array
 }
 
-//func even(array []int) []int {
-//	for 
-//}
+func even(array []int) []int {
+	for i := range array {
+		if array[i] % 2 == 0 {
+			array = remove(array, i)
+		}
+	} 
+
+	return array
+}
