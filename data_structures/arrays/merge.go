15795d39aa73656fdea4bebc8234d50a16be462e
Himanshu <sagarhimanshu472@gmail.com>
2025-01-12 09:59:12 +0530

    array: merging slices

--- data_structures/arrays/merge.go
+++ data_structures/arrays/merge.go
@@ -2,17 +2,13 @@ package main
 
 import "fmt"
 
-type ArrayItem interface {
-    int | float64 | string 
-}
-
 func main() {
     array := merge()
     fmt.Println(array)
 }
 
-func merge[T ArrayItem]() []T {
-    Array := []T{1, 2, 3, 4, 5}
-    Array = append(Array, []T{0, 1, 1, 1, 1, 0}...)
+func merge() []int {
+    Array := []int{1, 2, 3, 4, 5}
+    Array = append(Array, []int{0, 1, 1, 1, 1, 0}...)
     return Array
 }
