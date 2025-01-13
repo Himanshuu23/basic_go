0fcc948938a20c39b5823a7b047a3d26526b4dfc
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-13 22:06:58 +0530

    maximum element from the map

---
+++ data_structures/maps/maximum.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	students := make(map[string]int)
+	
+	students["a"] = 100
+	students["b"] = 200
+	students["c"] = 300
+	students["d"] = 400
+
+	maximum(students)
+}
+
+func maximum(Map map[string]int) {
+	var maximum string
+	val := -1e9
+
+	for key := range Map {
+		if Map[key] > val {
+			maximum = key
+		}
+	}
+
+	fmt.Println(maximum)
+}
