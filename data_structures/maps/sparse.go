9604761b9dd79c9749e6c40103388555a279d674
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-15 14:43:46 +0530

    sparse matrix creation, map with only non-zero element and their index and function to add and multiple

--- data_structures/maps/sparse.go
+++ data_structures/maps/sparse.go
@@ -12,6 +12,16 @@ func main() {
 	
 	Map := make(map[[2]int]int)
 	Array := make([][]int, row)
+	Matrix1 := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	Matrix2 := [][]int{
+		{10, 11, 12},
+		{13, 14, 15},
+		{16, 17, 18},
+	}
 
 	for i := range row {
 		Array[i] = make([]int, col)
@@ -20,8 +30,8 @@ func main() {
 		}
 	}
 
-	for i := 0; i < row; i++ {
-		for j := 0; j < col; j++ {
+	for i := range row {
+		for j := range col {
 			fmt.Scanln(&Array[i][j])
 			if Array[i][j] != 0 {
 				Map[[2]int{ i, j }] = Array[i][j]
@@ -29,5 +39,34 @@ func main() {
 		}
 	}
 
-	fmt.Println(Array, Map)
+	Print(Array)
+	add(Matrix1, Matrix2)
+	multiply(Matrix1, Matrix2)
+}
+
+func add(Array1, Array2 [][]int) {
+	for i := range Array1 {
+		for j := range Array2 {
+			fmt.Print(Array1[i][j] + Array2[i][j])	
+		}
+	}
+}
+
+func multiply(Array1, Array2 [][]int) {
+	for i := range Array1 {
+		result := 0
+		for j := range Array2 {
+			result += Array1[i][j]*Array2[j][i] 
+		}
+		fmt.Println(result)
+	}
+}
+
+func Print(Array [][]int) {
+	for i := range Array {
+		for j := range Array[i] {
+			fmt.Println(Array[i][j], " ")
+		}
+		fmt.Println("\n")
+	}
 }
