f3cab9bc29da2d94470805cd379e8e831540c6ea
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-19 15:41:17 +0530

    embedding two structs

--- oops/intermediate.go
+++ oops/intermediate.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
+type Animal struct {
+	sound	string
+}
+
+type Dog struct {
+	name	string
+	Animal
+}
+
 type Rectangle struct {
 	length		int
 	width		int
+}
+
+func (a Animal) PrintSound() {
+	fmt.Println(a.sound)
 }
 
 func NewRectangle(length, width int) *Rectangle {
@@ -22,4 +35,8 @@ func (r *Rectangle) Area() int {
 func main() {
 	r := NewRectangle(2, 3)
 	fmt.Println(r.Area())
+
+	a := Dog{name: "Shanky", Animal: Animal{sound: "bark"}}
+
+	a.PrintSound()
 }
