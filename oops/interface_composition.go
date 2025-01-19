95932b31ecb420a726d7b7bff90cfe869350aa5a
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-19 19:19:19 +0530

    multiple interfaces

---
+++ oops/interface_composition.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	Area() 		int
+}
+
+type Movable interface {
+	Move()		bool
+}
+
+type Car struct {
+	side 		int
+	canMove		bool
+}
+
+func (c Car) Area() int {
+	return c.side * c.side
+}
+
+func (c Car) Move() bool {
+	return c.canMove
+}
+
+func Area(s Shape) {
+	fmt.Println(s.Area())
+}
+
+func Move(m Movable) {
+	fmt.Println(m.Move())
+}
+
+func main() {
+	c := Car{2, false}
+	Area(c)
+	Movable(c)
+}
