9e230e8da177510738212931951528f236fe2404
Himanshu Sagar <sagarhimanshu472@gmail.com>
2025-01-20 21:29:10 +0530

    encoding-json

--- low_level_data/6.go
+++ low_level_data/6.go
@@ -11,13 +11,13 @@ type Person struct {
 }
 
 func main() {
-	json := `{"name": "something", "age": 32}`
+	jsonStr := `{"name": "something", "age": 32}`
 	p := Person{"something", 32}
 	var result map[string]any
-	result1, _ := json.Marshal([]byte(json))
-	result2, _ := json.MarshalIndent(p)
+	result1, _ := json.Marshal([]byte(jsonStr))
+	result2, _ := json.MarshalIndent(p, "", " ")
 	result3, _ := json.Marshal(map[string]int{"one": 1, "two": 2})
-	result4 := json.Unmarshal(json, &result)
+	json.Unmarshal([]byte(jsonStr), &result)
 
-	fmt.Println(result, result1, result2, result3, result4)
+	fmt.Println(result, string(result1), string(result2), string(result3))
 }
