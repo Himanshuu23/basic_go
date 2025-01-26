516112e5b57c96e63edb30d44b3b4dc9904f7750
Hi <sagarhimanshu472@gmail.com>
2025-01-26 18:34:23 +0530

    custom marshal and unmarshal

--- low_level_data/14.go
+++ low_level_data/14.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
 	"time"
 	"fmt"
 )
@@ -11,12 +9,27 @@ type Timestamp struct {
 	Time	time.Time
 }
 
-func (T Timestamp) MarshalJSON() string {
-	return T.Time
+func (T Timestamp) MarshalJSON() ([]byte, error) {
+	date := T.Time.Format("2006-01-02")
+	return []byte(`"` + date + `"`), nil
+}
+
+func (T *Timestamp) UnmarshalJSON(data []byte) error {
+    parsedTime, err := time.Parse("2006-01-02", string(data))
+    if err != nil {
+        return err
+    }
+    
+    T.Time = parsedTime
+    return nil
 }
 
 func main() {
 	t := Timestamp{Time: time.Now()}
-	something := t.MarshalJSON()
-	fmt.Println(something)
+	
+	marshal, _ := t.MarshalJSON()
+	fmt.Print(marshal)
+	
+	_ = t.UnmarshalJSON(marshal)
+	fmt.Println(t.Time)
 }
