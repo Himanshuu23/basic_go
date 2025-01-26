e95fd7b2c2bddbf35a489aa3817a32803fd5c498
Hi <sagarhimanshu472@gmail.com>
2025-01-26 14:32:43 +0530

    custom serialization and deserialization of the struct

--- low_level_data/11.go
+++ low_level_data/11.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"bytes"
-	"encoding/gob"
+	"encoding/binary"
 )
 
 type Employee struct {
@@ -11,25 +11,51 @@ type Employee struct {
 	Salary	float64
 }
 
-func main() {
-	p1 := Employee{1, 22.23}
-	var decoded Employee
+func serialize(emp Employee) ([]byte, error) {
+	buf := new(bytes.Buffer)
 
-	var buf bytes.Buffer
+	if err := binary.Write(buf, binary.BigEndian, emp.ID); err != nil {
+		return emp, err
+	)
 
-	encoder := gob.NewEncoder(&buf)
-	decoder := gob.NewDecoder(&buf)
-
-	if err := encoder.Encode(p1); err != nil {
-		fmt.Println(err)
-	}
+	if err := binary.Write(buf, binary.BigEndian, emp.Salary); err != nil {
+		return emp, err
+	)
 	
-	encoded := buf.Bytes()
+	return emp, err
+}
 
-	if err := decoder.Decode(&decoded); err != nil {
-		fmt.Println(err)
+func deserialize(data []byte) (emp Employee, error) {
+	buf := bytes.NewReader(data)
+	var emp Employee
+
+	if err := binary.Read(buf, binary.BigEndian, &emp.ID); err != nil {
+		return emp, err
+	)
+
+	if err := binary.Read(buf, binary.BigEndian, &emp.Salary); err != nil {
+		return emp, err
+	)
+	
+	return emp, err
+}
+
+func main() {
+	emp := Employee{ID: 1, Salary: 22.23}
+	
+	serializedData, err := serialize(p1)
+
+	if err != nil {
+		fmt.Println("Serialization error: ", err)
 	}
 
-	fmt.Println(encoded)
-	fmt.Println(decoded)
+	fmt.Println("Serialized Data: ", serializedData)
+
+	deserializedData, err := deserialized(serializedData)
+
+	if err != nil {
+		fmt.Println("Deserialization error: ", err)
+	}
+
+	fmt.Println("Deserialized error: ", deserializedData)
 }
