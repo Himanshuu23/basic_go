8c3863dafce135a24fc6b37f951192cd44c36367
Hi <sagarhimanshu472@gmail.com>
2025-01-26 17:49:46 +0530

    converting bigendian and littleendian to int32 and vice-versa

--- low_level_data/13.go
+++ low_level_data/13.go
@@ -16,4 +16,18 @@ func main() {
 	littleEndian := new(bytes.Buffer)
 	binary.Write(littleEndian, binary.LittleEndian, num)
 	fmt.Printf("Little Endian: %v\n", littleEndian.Bytes())
+
+	var x, y int32
+
+	err1 := binary.Read(bytes.NewReader(bigEndian.Bytes()), binary.BigEndian, &x)
+	if err1 != nil {
+		fmt.Print(err1)
+	}
+	fmt.Println("Integer for the Big Endian: ", x, "\n")
+	
+	err2 := binary.Read(bytes.NewReader(littleEndian.Bytes()), binary.LittleEndian, &y)
+	if err1 != nil {
+		fmt.Print(err2)
+	}
+	fmt.Println("Integer for the Little Endian: ", y, "\n")
 }
