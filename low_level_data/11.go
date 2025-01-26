65fa38958b17b8ecb22913cc1884344933e4da8e
Hi <sagarhimanshu472@gmail.com>
2025-01-26 14:06:08 +0530

    decoding employee

--- low_level_data/11.go
+++ low_level_data/11.go
@@ -6,21 +6,28 @@ import (
 	"encoding/gob"
 )
 
-type Person struct {
+type Employee struct {
 	ID	int
 	Salary	float64
 }
 
 func main() {
-	p1 := Person{1, 22.23}
+	p1 := Employee{1, 22.23}
+	var decoded Employee
 
 	var buf bytes.Buffer
 
 	encoder := gob.NewEncoder(&buf)
+	decoder := gob.NewDecoder(&buf)
 
 	if err := encoder.Encode(p1); err != nil {
 		fmt.Println(err)
 	}
+	
+	if err := decoder.Decode(&decoded); err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println(buf.Bytes())
+	fmt.Println(decoded)
 }
