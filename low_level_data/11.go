219f0ec1a87978e7d3028f69968e667faa83252c
Hi <sagarhimanshu472@gmail.com>
2025-01-26 14:55:38 +0530

    custom serialization and deserialization of structs

--- low_level_data/11.go
+++ low_level_data/11.go
@@ -22,28 +22,28 @@ func serialize(emp Employee) ([]byte, error) {
 		return buf.Bytes(), err
 	}
 	
-	return buf.Bytes(), err
+	return buf.Bytes(), nil
 }
 
-func deserialize(data []byte) (emp Employee, error) {
+func deserialize(data []byte) (Employee, error) {
 	buf := bytes.NewReader(data)
 	var emp Employee
 
 	if err := binary.Read(buf, binary.BigEndian, &emp.ID); err != nil {
 		return emp, err
-	)
+	}
 
 	if err := binary.Read(buf, binary.BigEndian, &emp.Salary); err != nil {
 		return emp, err
-	)
+	}
 	
-	return emp, err
+	return emp, nil
 }
 
 func main() {
 	emp := Employee{ID: 1, Salary: 22.23}
 	
-	serializedData, err := serialize(p1)
+	serializedData, err := serialize(emp)
 
 	if err != nil {
 		fmt.Println("Serialization error: ", err)
@@ -51,7 +51,7 @@ func main() {
 
 	fmt.Println("Serialized Data: ", serializedData)
 
-	deserializedData, err := deserialized(serializedData)
+	deserializedData, err := deserialize(serializedData)
 
 	if err != nil {
 		fmt.Println("Deserialization error: ", err)
