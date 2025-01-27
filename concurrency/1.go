fa52e50926e3a8c6753eeacb637c93596f38c21c
Hi <sagarhimanshu472@gmail.com>
2025-01-27 11:48:24 +0530

    go routines

--- concurrency/1.go
+++ concurrency/1.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	
+	wg.Add(2)
+	
+	go func() {
+	    Print()
+	    wg.Done()
+	}()
+	go func() {
+	    Print()
+	    wg.Done()
+	}()
+	
 	Print()
-	go Print()	
+	
+	wg.Wait()
 }
 
 func Print() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	    time.Sleep(time.Millisecond * 500)
 	}
 }
