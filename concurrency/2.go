58c1932d6ac5f01b87c31de84790de78bd9546b3
Hi <sagarhimanshu472@gmail.com>
2025-01-27 11:59:11 +0530

    go routine's id

---
+++ concurrency/2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"runtime"
+)
+
+func getGoroutineId() {
+	var buf [64]byte
+
+	n := runtime.Stack(buf[:], false)
+	id := 0
+	fmt.Sscanf(string(buf[:]), "goroutine %d", &id)
+	fmt.Println(id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(3)
+
+	go func() {
+		getGoroutineId()
+		wg.Done()
+	}()
+	
+	go func() {
+		getGoroutineId()
+		wg.Done()
+	}()
+	
+	go func() {
+		getGoroutineId()
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
