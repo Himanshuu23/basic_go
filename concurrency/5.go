9d4abbd0cceeec0b501bb467acf3d20cb9acb775
Hi <sagarhimanshu472@gmail.com>
2025-01-27 13:35:31 +0530

    fanit and fanout squares of the numbers

---
+++ concurrency/5.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main(){
+	var wg sync.WaitGroup
+	fanin := make(chan int, 10)
+	fanout := make(chan int, 10)
+
+	wg.Add(1)
+
+	go worker(fanin, fanout, &wg)
+
+	for i := 1; i <= 10; i++ {
+		fanin <- i
+	}
+
+	close(fanin)
+	wg.Wait()
+	close(fanout)
+
+	for i := range fanout {
+		fmt.Println(i)
+	}
+}
+
+func worker(fanin <-chan int, fanout chan<- int, wg *sync.WaitGroup){
+	for i := range fanin {
+		fanout <- square(i)
+		wg.Done()
+	}
+}
+
+func square(num int) int {
+	return num*num
+}
