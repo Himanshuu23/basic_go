e3218ee4ea9a3325eb12146f7677f47ba455a0da
Hi <sagarhimanshu472@gmail.com>
2025-01-27 13:00:48 +0530

    worker pool

--- concurrency/4.go
+++ concurrency/4.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
+    var wg sync.WaitGroup
 	producer := make(chan int, 20)
 	consumer := make(chan int, 20)
 
+    wg.Add(2)
+
+    go worker(producer, consumer, &wg)
+    go worker(producer, consumer, &wg)
+    
 	for i := 1; i <= 20; i++ {
 		producer <- i
 	}
 
-	producer.Close()
+	close(producer)
 
-	worker(producer, consumer)
-	
-	go func() {
-		fmt.println(<-consumer)
-	}()
-	
-	go func() {
-		fmt.println(<-consumer)
-	}()
-
-	consumer.Close()
+    wg.Wait()
+    close(consumer)
+    
+    for i := range consumer{
+	   fmt.Println(i)
+	}
 }
 
-func worker(producer <-chan int, consumer<- chan int) {
+func worker(producer <-chan int, consumer chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := range producer {
 		consumer <- i
 	}
