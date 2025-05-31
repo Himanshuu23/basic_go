b09e356f952fcf037c56a912f982a37342e28cc3
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-05-31 19:13:25 +0530

    feat: macros and boiler plate code for sol.cpp

--- data_structures/cp/sol.cpp
+++ data_structures/cp/sol.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+const int INT_MAX_VAL = INT_MAX;
+const int INT_MIN_VAL = INT_MIN;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)((x).size())
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
 int main() {
-    cout<<(0 ^ 1);
+    fastio;
+
+    // Your code starts here
+
     return 0;
 }
