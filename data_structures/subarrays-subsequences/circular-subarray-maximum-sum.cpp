b01b32c58edee5ccd077db08cfece9e4e1bdbce7
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-03 21:20:43 +0530

    feat: maximum circular subarray sum

---
+++ data_structures/subarrays-subsequences/circular-subarray-maximum-sum.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int kadane(vector<int>& arr, int n) {
+    int currSum = 0, maxSum = INT_MIN;
+
+    for (int i = 0; i < n; i++) {
+        currSum += arr[i];
+
+        if (currSum < 0) {
+            currSum = 0;
+        }
+
+        maxSum = max(maxSum, currSum);
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int n; cin >> n;
+    vector<int> arr;
+
+    for (int i = 0; i < n; i++) {
+        int temp = 0; cin >> temp;
+        arr.push_back(temp);
+    }
+
+    int totalSum = 0, wrapSum, nonWrapSum = kadane(arr, n);
+    
+    if (nonWrapSum < 0) { // when all the elements are negative
+        return nonWrapSum;
+    }
+
+    for (int i = 0; i < n; i++) {
+        totalSum += arr[i];
+        arr[i] = -arr[i];
+    }
+
+    wrapSum = totalSum + kadane(arr, n);
+    cout << max(wrapSum, nonWrapSum) << endl;
+    
+    return 0;
+}
