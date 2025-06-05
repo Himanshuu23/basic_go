5936a99afb23488f427c71959bcdd1594103fbe5
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-05 18:41:26 +0530

    printing rectangular pattern

---
+++ patterns/rectangular-pattern.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cin >> rows >> cols;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+         cout << "*" << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
