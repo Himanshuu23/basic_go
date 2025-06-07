5ef4326854a3e486f2a11a6f5bd5a546d27e24bc
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-07 20:45:36 +0530

    added spacing in the template

--- data_structures/cp/template.cpp
+++ data_structures/cp/template.cpp
@@ -1,21 +1,15 @@
+/*
+    author: Himanshuu23
+*/
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
-#define endl "\n"
-#define all(x) (x).begin(), (x).end()
-#define pb push_back
-#define fi first
-#define se second
-#define sz(x) (int)((x).size())
-
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-
 int main() {
-    fastio;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
 
     return 0;
 }
