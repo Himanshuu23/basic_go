c5d5426d64b9dec5fd46a31823d9407094e328f7
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-16 22:56:36 +0530

    feat: added testcases loop

--- data_structures/cp/template.cpp
+++ data_structures/cp/template.cpp
@@ -2,14 +2,16 @@
     author: Himanshuu23
 */
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int t; cin >> t;
+    while(t--) {
+
+    }
 
     return 0;
 }
