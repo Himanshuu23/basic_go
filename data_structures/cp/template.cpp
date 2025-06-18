8759482d8813dc50cc581c20abf1ab333e62894b
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-18 16:28:03 +0530

    feat: added long long macro

--- data_structures/cp/template.cpp
+++ data_structures/cp/template.cpp
@@ -3,12 +3,13 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t; cin >> t;
+    long t; cin >> t;
     while(t--) {
 
     }
