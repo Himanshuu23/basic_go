1ca1dbb4f58cca49c9868fb79fa14a0df7faef3d
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-22 14:36:52 +0530

    Problem - 1573 B

---
+++ data_structures/cp/codeforces/1033-Div-02/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int l1, b1, l2, b2, l3, b3;
+        cin >> l1 >> b1 >> l2 >> b2 >> l3 >> b3;
+
+        int side = max({l1, l2, l3, b1, b2, b3});
+
+        if (l1 == l2 && l2 == l3 && b1 + b2 + b3 == l1)
+            cout << "YES\n";
+        else if (b1 == b2 && b2 == b3 && l1 + l2 + l3 == b1)
+            cout << "YES\n";
+        else if (l1 == l2 + l3 && b2 == b3 && b1 == b2 + b3 && l2 == l3)
+            cout << "YES\n";
+        else if (b1 == b2 + b3 && l2 == l3 && l1 == l2 + l3 && b2 == b3)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
