9b03d3f807a1d74c90502a148a63c5fb7c1cebff
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-06-26 18:44:12 +0530

    all possible way in two dimensional board

--- data_structures/recursion/possible-paths-one-d.cpp
+++ data_structures/recursion/possible-paths-one-d.cpp
@@ -5,11 +5,14 @@
 using namespace std;
 typedef long long ll;
 
-void printPossible(int start, int end, int &ans) {
+void printPossible(int start, int end, int &ans, string path) {
     if (start > end) return;
-    if (start == end) ++ans;
+    if (start == end) {
+        cout << path << endl;
+        ++ans;
+    }
     for (int i = 1; i <= 6; i++) {
-        printPossible(start + i, end, ans);
+        printPossible(start + i, end, ans, path + to_string(i));
     }
 }
 
@@ -20,8 +23,9 @@ int main() {
     long t; cin >> t;
     while(t--) {
         int n, ans(0); cin >> n;
-        printPossible(0, n, ans);
-        cout << ans << endl;
+        string path;
+        printPossible(0, n, ans, path);
+        cout << "Total possible ways : " << ans << endl;
     }
 
     return 0;
