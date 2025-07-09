56a3f29724424781f6e36519c02a941ba3f57e3c
Himanshuu23 <sagarhimanshu472@gmail.com>
2025-07-09 23:07:01 +0530

    reversing a queue

---
+++ data_structures/queue/reverse-queue.cpp
@@ -0,0 +1,47 @@
+/*
+    author: Himanshuu23
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve(queue<int>& q) {
+    stack<int> help;
+    while (!q.empty()) {
+        help.push(q.front());
+        q.pop();
+    }
+    while (!help.empty()) {
+        q.push(help.top());
+        help.pop();
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long t; cin >> t;
+    while(t--) {
+        queue<int>q;
+        q.push(1);
+        q.push(2);
+        q.push(3);
+
+        cout << q.front() << endl; q.pop();
+        cout << q.front() << endl; q.pop();
+        cout << q.front() << endl; q.pop();
+        
+        q.push(1);
+        q.push(2);
+        q.push(3);
+
+        solve(q);
+        
+        cout << q.front() << endl; q.pop();
+        cout << q.front() << endl; q.pop();
+        cout << q.front() << endl; q.pop();
+    }
+
+    return 0;
+}
